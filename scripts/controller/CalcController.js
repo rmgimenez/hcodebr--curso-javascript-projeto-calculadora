7711d092606684c500d5b530462ee3cba29ba2de
Ricardo Moura Gimenez <rmgimenez@gmail.com>
2020-08-21 22:35:30 -0300

    aplicando varios eventos usando o split

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,70 +1,84 @@
 class CalcController {
-  constructor() {
-    this._locale = "pt-BR";
-    this._displayCalcEl = document.querySelector("#display");
-    this._dateEl = document.querySelector("#data");
-    this._timeEl = document.querySelector("#hora");
+    constructor() {
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
 
-    this._currentDate;
+        this._currentDate;
 
-    this.initialize();
-    this.initButtonsEvents();
-  }
+        this.initialize();
+        this.initButtonsEvents();
+    }
 
-  initialize() {
-    this.setDisplayDateTime();
+    initialize() {
+        this.setDisplayDateTime();
 
-    setInterval(() => {
-      this.setDisplayDateTime();
-    }, 1000);
-  }
+        setInterval(() => {
+            this.setDisplayDateTime();
+        }, 1000);
+    }
 
-  initButtonsEvents() {
-    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+    addEventListenerAll(element, events, fn) {
+        events.split(" ").forEach((event) => {
+            element.addEventListener(event, fn, false);
+        });
+    }
 
-    buttons.forEach((btn, index) => {
-      btn.addEventListener("click", (e) => {
-        console.log(btn.className.baseVal.replace('btn-', ''));
-      });
-    });
-  }
+    initButtonsEvents() {
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
-  setDisplayDateTime() {
-    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
-      day: "2-digit",
-      month: "long",
-      year: "numeric",
-    });
-    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
-  }
+        buttons.forEach((btn, index) => {
+            this.addEventListenerAll(btn, "click", (e) => {
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
 
-  get displayTime() {
-    return this._timeEl.innerHTML;
-  }
-  set displayTime(value) {
-    this._timeEl.innerHTML = value;
-  }
+            this.addEventListenerAll(
+                btn,
+                "mouseover mouseup mousedown",
+                (e) => {
+                    btn.style.cursor = "pointer";
+                }
+            );
+        });
+    }
 
-  get displayDate() {
-    return this._dateEl.innerHTML;
-  }
-  set displayDate(value) {
-    this._dateEl.innerHTML = value;
-  }
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
 
-  get displayCalc() {
-    return this._displayCalcEl.innerHTML;
-  }
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
 
-  set displayCalc(value) {
-    this._displayCalcEl.innerHTML = value;
-  }
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
 
-  get currentDate() {
-    return new Date();
-  }
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML;
+    }
 
-  set currentDate(value) {
-    this._currentDate = value;
-  }
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
+    }
 }
